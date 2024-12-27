738d8ad947fd3114b7520b1be752a559f17397da
HBpencil <71178397+warlockhb@users.noreply.github.com>
2024-12-27 17:33:04 +0900

    Create 005_Compare_Two_Numbers.cpp

---
+++ CodeKata/CodeKata/005_Compare_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    if (num1 == num2)
+    {
+        int answer = 1;
+        return answer;
+    }
+    
+    
+    int answer = -1;
+    return answer;
+}
