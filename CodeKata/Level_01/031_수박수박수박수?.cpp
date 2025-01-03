60b23627dbb32035098581eec9c37e99463a0503
HBpencil <71178397+warlockhb@users.noreply.github.com>
2025-01-03 10:14:30 +0900

    Create 031_수박수박수박수?.cpp

---
+++ CodeKata/Level_01/031_수박수박수박수?.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) 
+{
+    string answer = "";
+    for (int i = 1; i <= n; i++)
+        answer += (i % 2 != 0 ? "수" : "박");
+    
+    return answer;
+}
